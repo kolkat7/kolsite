b0b8bca3cc190d5e2bb15adb82b8c989a423d15e
kolkat <61978243+kolkat7@users.noreply.github.com>
2021-03-14 01:27:33 -0300

    Add files via upload

---
+++ first.js
@@ -0,0 +1,18 @@
+function gaming(){
+        alert("STOP\nDO NOT CONTINUE");
+    }
+function redirect(){
+    window.location.href= "https://www.destiny.gg/bigscreen";
+}
+function memered(){
+    window.location.href= "https://www.4chan.org/d";
+}
+function twitterred(){
+    window.location.href= "https://www.twitter.com/kolkat7";
+}
+
+function nothing(){
+    var fnt 
+    for(fnt = 1;fnt<6699 ; fnt++);
+    alert("look number " + fnt);
+}
